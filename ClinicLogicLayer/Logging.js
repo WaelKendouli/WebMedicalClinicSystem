91a20c018bcbf75512b9c2112fea1eeb8eee5805
WaelKendouli <kendouliwael@gmail.com>
2026-09-15 09:43:16 +0200

    Adding UI object

---
+++ ClinicLogicLayer/Logging.js
@@ -0,0 +1,7 @@
+const User = {
+    Username : document.getElementById("username").value ,
+    Password : document.getElementById("password").value ,
+    Email : document.getElementById("email").value,
+    Phone : document.getElementById("phone").value
+}
+
