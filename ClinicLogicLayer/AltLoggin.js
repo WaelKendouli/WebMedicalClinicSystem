77f4073e289b57735b1db97b18d4032ac423aa6f
WaelKendouli <kendouliwael@gmail.com>
2026-09-17 11:11:41 +0200

    Adding the alternative logging file

---
+++ ClinicLogicLayer/AltLoggin.js
@@ -0,0 +1,84 @@
+const UI = {
+    ErrorMessage : document.getElementById("lbInvalid"),
+    frmLogin : document.getElementById("loginForm")
+}
+function SetError(message)
+{
+    if (!message) {
+        UI.ErrorMessage.style.display = "flex";
+        UI.ErrorMessage.textContent = message;
+        return;
+    }
+    UI.ErrorMessage.style.display = "none";
+}
+
+function CheckInputs(value, message) {
+    if (!value || !value.trim()) {
+        SetError(message);
+        return false;
+    }
+    return true;
+}
+
+const API_Base = "http://localhost:5202/api/Clinic";
+
+function LinkShow(elm)
+{
+ if(elm.checked)
+ {
+    elm.checked = false;
+ }
+ else
+ {
+    elm.checked = true;
+ }
+
+}
+
+async function SecondaryAuthenticationMethod( email , phone )
+{
+     if (!CheckInputs(email, "Invalid username") ||
+        !CheckInputs(phone, "Invalid password")) {
+        return false;
+    }
+    SetError("");
+
+    try {
+        const res = await fetch(`${API_Base}/Sec-auth`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({ email, phone })
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            SetError(data.message || `HTTP ${res.status}`);
+            return false;
+        }
+
+        return true;
+    } catch (e) {
+        SetError(e.message || "Network error");
+        return false;
+    }
+}
+
+
+
+UI.frmLogin.addEventListener("submit" ,async (event)=> {
+event.preventDefault();
+const email = document.getElementById("email").value 
+ const   phone = document.getElementById("phone").value 
+ const res = await PrimaryAuthenticationMethod(Username ,Password);
+if (res) {
+    window.location.href = 'index.html';
+}
+else
+{
+    SetError("your account does'nt exist as an admin");
+}
+});
