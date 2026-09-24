37d251a5f82616047abe701cb12d94cc2b776116
WaelKendouli <kendouliwael@gmail.com>
2026-09-24 22:08:27 +0200

    addingget all patients method

--- ClinicLogicLayer/Patients.js
+++ ClinicLogicLayer/Patients.js
@@ -17,3 +17,32 @@ const myDialog = document.getElementById("diagAdd");
     });
 
     const BASE_API = "http://localhost:5202/api/Patient";
+
+    let liPatients ; // list of patients for caching +
+    //  searching without depending on the server in some palces of code
+
+    async function GetAllPatients()
+    {
+        try {
+        const res = await fetch(`${API_Base}/GetAllPatients` , {
+             method: "GET",
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+        if (res.ok) {
+            const data = await res.json();
+            liPatients = data;
+            return data;
+        }
+        else
+        {
+            throw new Error("couldn't get the data from the server");
+        }
+        }
+        catch(e)
+        {
+            console.log(e.message);
+            return null;
+        }
+    }
